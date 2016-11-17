8e8296873981921ef35a119be4de7009a51008f8
teitei-tk <teitei.tk@gmail.com>
2016-11-17 23:07:08 +0900

    fetch all tawawa tweets

--- twitter/request.go
+++ twitter/request.go
@@ -6,6 +6,8 @@ import (
 	"strconv"
 	"time"
 
+	"github.com/ChimeraCoder/anaconda"
+
 	"github.com/teitei-tk/tawawa-bot/config"
 )
 
@@ -18,7 +20,7 @@ const (
 	defaultFindTimelineCount = 200
 
 	TwitterResponseCacheKey = "Tawawa"
-	ResponseCacheExpiredAt  = 1
+	ResponseCacheExpiredAt  = 1000000000
 )
 
 type RequestParametor struct {
@@ -30,7 +32,7 @@ type RequestParametor struct {
 }
 
 // Get Tawawa Owner Timeline it's use Cache
-func GetOwnerTimeline(client Client, param RequestParametor) (res UserTimelineResponse, err error) {
+func GetAllTawawaTweets(client Client, param RequestParametor) (res UserTimelineResponse, err error) {
 	res = UserTimelineResponse{}
 
 	redisClient := config.InitRedis()
@@ -43,10 +45,11 @@ func GetOwnerTimeline(client Client, param RequestParametor) (res UserTimelineRe
 		return res, err
 	}
 
-	res, err = getOwnerTimelineFromTwitter(client, param)
+	result, err := FetchAllTawawaTweets(client, param)
 	if err != nil {
 		return res, err
 	}
+	res.Tweets = result
 
 	bytes, err := json.Marshal(res)
 	if err != nil {
@@ -69,7 +72,7 @@ func getOwnerTimelineFromTwitter(client Client, param RequestParametor) (res Use
 		ScreenName:      OwnerScreenName,
 		Count:           defaultFindTimelineCount,
 		IncludeReTweets: false,
-		SinceID:         sinceTweetID,
+		SinceID:         param.SinceID,
 		MaxID:           param.MaxID,
 	}
 
@@ -77,7 +80,11 @@ func getOwnerTimelineFromTwitter(client Client, param RequestParametor) (res Use
 	values.Add("screen_name", mergedParam.ScreenName)
 	values.Add("count", strconv.Itoa(mergedParam.Count))
 	values.Add("include_rts", strconv.FormatBool(mergedParam.IncludeReTweets))
-	values.Add("since_id", strconv.FormatInt(mergedParam.SinceID, 10))
+	if param.SinceID != 0 {
+		values.Add("since_id", strconv.FormatInt(mergedParam.SinceID, 10))
+	} else {
+		values.Add("since_id", strconv.FormatInt(sinceTweetID, 10))
+	}
 
 	if param.MaxID != 0 {
 		values.Add("max_id", strconv.FormatInt(mergedParam.MaxID, 10))
@@ -90,4 +97,74 @@ func getOwnerTimelineFromTwitter(client Client, param RequestParametor) (res Use
 
 	res.Tweets = results
 	return res, nil
+}
+
+func showFromOwnerTweet(tweetID int64, client Client) (resTweet anaconda.Tweet, err error) {
+	var tweet anaconda.Tweet
+
+	values := make(url.Values)
+	values.Add("id", strconv.FormatInt(tweetID, 10))
+	values.Add("include_entities", strconv.FormatBool(true))
+
+	res, err := client.APIClient.GetTweet(tweetID, values)
+	if err != nil {
+		return tweet, err
+	}
+
+	return res, nil
+}
+
+func FetchAllTawawaTweets(client Client, param RequestParametor) (filterdTweets []anaconda.Tweet, err error) {
+	var maxRequestTweetID int64
+	var sinceID int64
+	var tweestCount int
+	var tweets []anaconda.Tweet
+
+	for {
+		if maxRequestTweetID != 0 {
+			param.MaxID = maxRequestTweetID
+		}
+
+		res, err := getOwnerTimelineFromTwitter(client, param)
+		tweestCount = tweestCount + len(res.Tweets)
+		if err != nil {
+			return tweets, err
+		}
+
+		filterdRes := FilterTawawaTweets(res)
+		for _, t := range filterdRes.Tweets {
+			if maxRequestTweetID == 0 || maxRequestTweetID > t.Id {
+				maxRequestTweetID = t.Id
+			}
+
+			if sinceID == 0 || t.Id > sinceID {
+				sinceID = t.Id
+			}
+
+			tweets = append(tweets, t)
+		}
+
+		if maxRequestTweetID == sinceTweetID || tweestCount >= 3200 {
+			break
+		}
+	}
+
+	var showTweetRequestCount = 0
+	var requestTweetID = maxRequestTweetID
+	for {
+		resTweet, err := showFromOwnerTweet(requestTweetID, client)
+		if err != nil {
+			return tweets, err
+		}
+
+		tweets = append(tweets, resTweet)
+
+		requestTweetID = resTweet.InReplyToStatusID
+		showTweetRequestCount = showTweetRequestCount + 1
+		if showTweetRequestCount >= 100 || resTweet.Id == sinceTweetID {
+			break
+		}
+	}
+
+	return tweets, nil
 }
